784e956eac017a806100815801e53c7a506200ba
Russell Matney <russell.matney@gmail.com>
2013-12-11 23:57:44 -0800

    switches get to jsonp with callback string. adds some comments up in herr.

--- app/scripts/controllers/zipForm.js
+++ app/scripts/controllers/zipForm.js
@@ -8,13 +8,31 @@ angular.module('weatherbyApp')
   $scope.submit = function(zip) {
   	$scope.weather = null;
 
-  	$http({method: 'GET', url: 'http://api.wunderground.com/api/4227e08d470f4fe8/forecast/q/'+zip.code+'.json'})
-  		.success(function(forecast){
+    // Seems jsonp never comes up except in small side projects - your gut with the 'GET' was more correct.
+    // Only problem is the Access-Control-Allow-Origin errors, which are related to CORS
+    // (Cross-origin Rsomething Ssomething) which is too complicated to worry about for now.
+    // The short answer is that the origin and destination need to agree on some headers...we can look into it later. 
+    // for now, jsonp will be easier, just comes with this goofy ?callback=JSON_CALLBACK string.
+  	$http.jsonp('http://api.wunderground.com/api/4227e08d470f4fe8/forecast/q/'+zip.code+'.json?callback=JSON_CALLBACK')
+  		//i normally don't agree with shortening variable names, but res = response is pretty universal
+      .success(function(res){
+        console.log('success');
+        
+        // do with this what you will :)
+        var forecast = res.forecast;
+        console.log(forecast);
+
+        // console.log-ing this shows forcastday is an array, so you can't pull fctext directly from it,
+        // you'll need forecastday[0].fctext or another implementation (ng-repeat maybe? or were you already thinking that?)
   			$scope.weather = forecast.txt_forecast.forecastday.fctext;
   		})
-  		.error(function(data,status){
-  			$scope.weather = data || "Request failed";
-  			$scope.status = status;
+      // really glad you handled this error :)
+  		.error(function(error){
+        console.log('error');
+        console.log(error);
+
+  			$scope.weather = error.message || "Request failed";
+  			$scope.status = error.status;
   		})
 
   };
