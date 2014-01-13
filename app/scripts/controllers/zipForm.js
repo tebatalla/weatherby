8ad08d500e873d9bd216972bfba5501d5b48dcbd
Tomas <tebatalla@gmail.com>
2014-01-12 23:07:10 -0600

    New service

--- app/scripts/controllers/zipForm.js
+++ app/scripts/controllers/zipForm.js
@@ -1,33 +1,56 @@
 'use strict';
+angular.module('weatherbyApp', ['ngRoute']).provider('Weather', function() {
+
+	var apiKey = '';
+
+	this.setApiKey = function(key) {
+		if (key) this.apiKey = key;
+	};
+
+	this.getUrl = function(type, ext) {
+		return 'http://api.wunderground.com/api/' + 
+		this.apiKey + '/' + type + '/q/' +
+		ext + '.json';
+	};
+
+	this.$get = function($q, $http) {
+		var self = this;
+			return {
+			getWeatherForecast: function(zipCode) {
+				var d = $q.defer();
+				$http({
+					method: 'GET',
+					url: self.getUrl('forecast', zipCode),
+					cache: true
+					}).success(function(data) {
+					// The wunderground API returns the 
+					// object that nests the forecasts inside
+					// the forecast.simpleforecast key
+					d.resolve(data.forecast.simpleforecast);
+					}).error(function(err) {
+					d.reject(err);
+					});
+				return d.promise;
+			}
+		};
+	};
+})
+.config(function(WeatherProvider) {
+  WeatherProvider.setApiKey('4227e08d470f4fe8');
+})
 
 angular.module('weatherbyApp')
-  .controller('ZipFormCtrl', function($scope, $http) {
+  .controller('ZipFormCtrl', ['Weather','$scope', '$http', function($scope, $http, Weather) {
 
   $scope.master = {};
 
   $scope.submit = function(zip) {
-    $scope.weather = null;
-
-    var onSuccess = function(res){
-      console.log('success');
-      var forecast = res.forecast;
-      console.log(forecast);
-      $scope.weather = forecast.txt_forecast;
-    };
-
-    var onError = function(error) {
-      console.log('error');
-      console.log(error);
-
-      $scope.weather = error.message || "Request failed";
-      $scope.status = error.status;
-    }
-
-    $http.jsonp('http://api.wunderground.com/api/4227e08d470f4fe8/forecast/q/'+zip.code+'.json?callback=JSON_CALLBACK')
-      .success(onSuccess).error(onError);
-
-
-
+  	
+  	$scope.weather = {}
+    Weather.getWeatherForecast(zip)
+    .then(function(data) {
+      $scope.weather.forecast = data;
+    });
   };
 
   $scope.reset = function() {
