f5363c34f83afb1a44592176460f0f4a3e52cbdb
Russell Matney <russell.matney@gmail.com>
2014-01-13 01:36:02 -0500

    refactor/debugging over video chat

--- app/scripts/controllers/zipForm.js
+++ app/scripts/controllers/zipForm.js
@@ -1,62 +1,85 @@
 'use strict';
-angular.module('weatherbyApp', ['ngRoute']).provider('Weather', function() {
+angular.module('weatherbyApp').provider('Weather', function() {
 
-	var apiKey = '';
+  var apiKey = '';
 
-	this.setApiKey = function(key) {
-		if (key) this.apiKey = key;
-	};
+  this.setApiKey = function(key) {
+    if(key) {
+      this.apiKey = key;
+    }
+  };
 
-	this.getUrl = function(type, ext) {
-		return 'http://api.wunderground.com/api/' + 
-		this.apiKey + '/' + type + '/q/' +
-		ext + '.json';
-	};
+  this.getUrl = function(type, ext) {
+    var returnString = "";
+    returnString += 'http://api.wunderground.com/api/' + this.apiKey 
+    returnString += '/' + type + '/q/' + ext + '.json';
+    return returnString;
+  };
 
-	this.$get = function($q, $http) {
-		var self = this;
-			return {
-			getWeatherForecast: function(zipCode) {
-				var d = $q.defer();
-				$http({
-					method: 'GET',
-					url: self.getUrl('forecast', zipCode),
-					cache: true
-					}).success(function(data) {
-					// The wunderground API returns the 
-					// object that nests the forecasts inside
-					// the forecast.simpleforecast key
-					d.resolve(data.forecast.simpleforecast);
-					}).error(function(err) {
-					d.reject(err);
-					});
-				return d.promise;
-			}
-		};
-	};
+  this.$get = function($q, $http) {
+    var self = this;
+      return {
+        format: function() {
+          console.log('format');
+        },
+        getWeatherForecast: function(location) {
+        var deferred = $q.defer();
+
+
+
+        $http({
+          method: 'JSONP',
+          url: self.getUrl('forecast', location.zipCode) + "?callback=JSON_CALLBACK",
+          cache: true
+          }).success(function(data) {
+            // The wunderground API returns the 
+            // object that nests the forecasts inside
+            // the forecast.simpleforecast key
+            console.log('getWeather')
+            deferred.resolve(data.forecast.simpleforecast);
+
+          }).error(function(err) {
+            deferred.reject(err);
+
+          });
+
+        return deferred.promise;
+      }
+    };
+  };
 })
 .config(function(WeatherProvider) {
   WeatherProvider.setApiKey('4227e08d470f4fe8');
-})
+});
 
 angular.module('weatherbyApp')
-  .controller('ZipFormCtrl', ['Weather','$scope', '$http', function($scope, $http, Weather) {
+  .controller('ZipFormCtrl', ['$scope', '$http', 'Weather', function($scope, $http, Weather) {
 
-  $scope.master = {};
+  $scope.defaultLocation = {
+    code: "",
+    name: ""
+  };
 
-  $scope.submit = function(zip) {
-  	
-  	$scope.weather = {}
-    Weather.getWeatherForecast(zip)
-    .then(function(data) {
+  $scope.submit = function(location) {
+    $scope.weather = {};
+
+    var onSuccess = function(data) {
       $scope.weather.forecast = data;
-    });
+      Weather.format($scope.weather.forecast)
+    };
+
+    var onError = function(err) {
+      console.log(err);
+    };
+
+    Weather.getWeatherForecast(location)
+      .then(onSuccess, onError);
   };
 
   $scope.reset = function() {
-    $scope.zip = angular.copy($scope.master);
+    $scope.location = angular.copy($scope.defaultLocation);
     $scope.weather = null;
   };
 
   $scope.reset();
-});
+}]);
