48c1deaeefb1f4dc9f487d33b34917e15b9dbd37
Tomas <tebatalla@gmail.com>
2014-01-14 23:59:04 -0600

    :Added formatWeather service

--- app/scripts/controllers/zipForm.js
+++ app/scripts/controllers/zipForm.js
@@ -1,85 +1,104 @@
 'use strict';
-angular.module('weatherbyApp').provider('Weather', function() {
 
-  var apiKey = '';
+angular.module('weatherbyApp').provider('Weather', function () {
 
-  this.setApiKey = function(key) {
-    if(key) {
-      this.apiKey = key;
-    }
-  };
+    var apiKey = '';
 
-  this.getUrl = function(type, ext) {
-    var returnString = "";
-    returnString += 'http://api.wunderground.com/api/' + this.apiKey 
-    returnString += '/' + type + '/q/' + ext + '.json';
-    return returnString;
-  };
-
-  this.$get = function($q, $http) {
-    var self = this;
-      return {
-        format: function() {
-          console.log('format');
-        },
-        getWeatherForecast: function(location) {
-        var deferred = $q.defer();
-
-
-
-        $http({
-          method: 'JSONP',
-          url: self.getUrl('forecast', location.zipCode) + "?callback=JSON_CALLBACK",
-          cache: true
-          }).success(function(data) {
-            // The wunderground API returns the 
-            // object that nests the forecasts inside
-            // the forecast.simpleforecast key
-            console.log('getWeather')
-            deferred.resolve(data.forecast.simpleforecast);
-
-          }).error(function(err) {
-            deferred.reject(err);
-
-          });
-
-        return deferred.promise;
-      }
+    this.setApiKey = function (key) {
+        if (key) {
+            this.apiKey = key;
+        }
     };
-  };
+
+    this.getUrl = function (type, ext) {
+        var returnString = "";
+        returnString += 'http://api.wunderground.com/api/' + this.apiKey
+        returnString += '/' + type + '/q/' + ext + '.json';
+        return returnString;
+    };
+
+    this.$get = function ($q, $http, formatWeather) {
+        var self = this;
+        return {
+            getWeatherForecast: function (location) {
+                var deferred = $q.defer();
+
+
+
+                $http({
+                    method: 'JSONP',
+                    url: self.getUrl('hourly', location.zipCode) + "?callback=JSON_CALLBACK",
+                    cache: true
+                }).success(function (data) {
+                    // The wunderground API returns the 
+                    // object that nests the forecasts inside
+                    // the forecast.simpleforecast key
+                    console.log(data);
+                    deferred.resolve(formatWeather.format(data));
+
+                }).error(function (err) {
+                    deferred.reject(err);
+
+                });
+
+                return deferred.promise;
+                //return (deferred.promise);
+            }
+        };
+    };
 })
-.config(function(WeatherProvider) {
-  WeatherProvider.setApiKey('4227e08d470f4fe8');
-});
+    .config(function (WeatherProvider) {
+        WeatherProvider.setApiKey('4227e08d470f4fe8');
+    });
 
 angular.module('weatherbyApp')
-  .controller('ZipFormCtrl', ['$scope', '$http', 'Weather', function($scope, $http, Weather) {
+	.service('formatWeather', function(){
+		return {
+            format: function (data) {
+                if (data.hourly_forecast[0].snow.english > 0) {
+                    return 'It\'s going to snow! Don\'t forget to wear some snow boots and a winter jacket!';
+                }
+                else if (data.hourly_forecast[0].feelslike.english < 20 && data.hourly_forecast[0].wspd.english >= 15) {
+                    return 'It\'s going to be cold AND windy. Wear several layers, with a windbreaker to withstand the biting wind!';
+                }
+                else{
+                	return 'Damn son, it\'s gonna be cloudy... I think...'
+                }
 
-  $scope.defaultLocation = {
-    code: "",
-    name: ""
-  };
+            }
+		}
+	});
 
-  $scope.submit = function(location) {
-    $scope.weather = {};
+angular.module('weatherbyApp')
+	.controller('ZipFormCtrl', ['$scope', '$http', 'Weather', function ($scope, $http, Weather){
 
-    var onSuccess = function(data) {
-      $scope.weather.forecast = data;
-      Weather.format($scope.weather.forecast)
-    };
+        $scope.defaultLocation = {
+            code: "",
+            name: ""
+        };
 
-    var onError = function(err) {
-      console.log(err);
-    };
 
-    Weather.getWeatherForecast(location)
-      .then(onSuccess, onError);
-  };
+        $scope.submit = function (location) {
 
-  $scope.reset = function() {
-    $scope.location = angular.copy($scope.defaultLocation);
-    $scope.weather = null;
-  };
 
-  $scope.reset();
-}]);
+            $scope.weather = {};
+
+            var onSuccess = function (data) {
+                $scope.weather.forecast = data;
+                console.log(data);
+            };
+
+            var onError = function (err) {
+                console.log(err);
+            };
+
+            Weather.getWeatherForecast(location)
+                .then(onSuccess, onError);
+        };
+
+        $scope.reset = function () {
+            $scope.location = angular.copy($scope.defaultLocation);
+            $scope.weather = null;
+        };
+
+    }]);
